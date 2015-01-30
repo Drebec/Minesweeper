d66346ab85b1e03c84152b61151ee0c517feac5f
Drebec <drew.beck33@gmail.com>
2015-01-30 00:09:52 -0800

    First Upload

---
+++ src/outlab1/Minesweeper.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outlab1;
+
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Drew Beck
+ */
+public class Minesweeper extends JFrame {
+
+    private static SweeperPanel[][] board;
+    private int width;
+    private int height;
+
+    public Minesweeper(int width, int height) {
+        super("Minesweeper");
+        this.width = width;
+        this.height = height;
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        getContentPane().setLayout(new GridLayout(width, height));
+
+        board = new SweeperPanel[width][height];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                SweeperPanel spot = new SweeperPanel(40, i, j);
+                board[i][j] = spot;
+                getContentPane().add(spot);
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (!board[i][j].getMine()) {
+                    board[i][j].setNumMines(String.valueOf(setNums(i, j)));
+                }
+            }
+        }
+
+        pack();
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+
+    public static SweeperPanel[][] getBoard() {
+        return board;
+    }
+
+    public int setNums(int i, int j) {
+        int counter = 0;
+
+        for (int x = (i - 1); x <= (i + 1); x++) {
+            for (int y = (j - 1); y <= (j + 1); y++) {
+                if (x >= 0 && x < board.length && y >= 0 && y < board[0].length) {
+                    if (board[x][y].getMine()) {
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        int choice = JOptionPane.showConfirmDialog(null, "Would you like to use the standard 10 by 10 board size?");
+        if (choice == 0) {
+            Minesweeper m1 = new Minesweeper(10, 10);
+        } else if (choice == 1) {
+            boolean play = false;
+            String sizeString;
+            int size = 0;
+            while (!play) {
+                try {
+                    sizeString = (JOptionPane.showInputDialog(null, "What would you like the board size to be?\nMore than 25 may not fit on your screen."));
+                    if (sizeString == null) {
+                        System.exit(0);
+                    }
+                    size = Integer.parseInt(sizeString);
+                    while (size < 5 || size > 25) {
+                        JOptionPane.showMessageDialog(null, "Please enter a number from 5 to 25");
+                        sizeString = (JOptionPane.showInputDialog(null, "What would you like the board size to be?\nEnter a number between 5 and 25."));
+                        if(sizeString == null) {
+                            System.exit(0);
+                        }
+                        size = Integer.parseInt(sizeString);
+                    }
+                    Minesweeper m2 = new Minesweeper(size, size);
+                    play = true;
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Please enter an integer value");
+                }
+            }
+        } else {
+            System.exit(0);
+        }
+    }
+}
---
+++ src/outlab1/SweeperPanel.java
@@ -0,0 +1,246 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outlab1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Drew
+ */
+public class SweeperPanel extends JPanel {
+
+    private boolean mine, flagged, clicked;
+    private int size, x, y;
+    private String numMines;
+
+    public SweeperPanel(int size, int x, int y) {
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        flagged = false;
+        clicked = false;
+        setLayout(new BorderLayout());
+
+        Dimension dims = new Dimension(size, size);
+        setPreferredSize(dims);
+        setMaximumSize(dims);
+        setMinimumSize(dims);
+
+        addMouseListener(new MSMouseListener());
+        int rand = (int) (Math.random() * 8);
+
+        if (rand == 0) {
+            mine = true;
+        } else {
+            mine = false;
+        }
+    }
+
+    public boolean getMine() {
+        return mine;
+    }
+
+    public boolean getClicked() {
+        return clicked;
+    }
+
+    public void setClicked() {
+        clicked = true;
+    }
+
+    public boolean getFlagged() {
+        return flagged;
+    }
+
+    public void setFlagged() {
+        flagged = true;
+    }
+
+    public void setNumMines(String s) {
+        numMines = s;
+    }
+
+    public void lose() {
+        for (int i = 0; i < Minesweeper.getBoard().length; i++) {
+            for (int j = 0; j < Minesweeper.getBoard()[i].length; j++) {
+                if (Minesweeper.getBoard()[i][j].getMine()) {
+                    Minesweeper.getBoard()[i][j].setClicked();
+                    Minesweeper.getBoard()[i][j].repaint();
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(this, "You Hit A Mine!");
+        System.exit(0);
+    }
+
+    public boolean win() {
+        for (int i = 0; i < Minesweeper.getBoard().length; i++) {
+            for (int j = 0; j < Minesweeper.getBoard()[i].length; j++) {
+                if (!Minesweeper.getBoard()[i][j].getClicked() && !Minesweeper.getBoard()[i][j].getMine()) {
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < Minesweeper.getBoard().length; i++) {
+            for (int j = 0; j < Minesweeper.getBoard()[i].length; j++) {
+                if (Minesweeper.getBoard()[i][j].getMine()) {
+                    Minesweeper.getBoard()[i][j].setFlagged();
+                    Minesweeper.getBoard()[i][j].repaint();
+                }
+            }
+        }
+        return true;
+    }
+
+    public void flood(int a, int b) {
+        for (int i = (a - 1); i <= (a + 1); i++) {
+            for (int j = (b - 1); j <= (b + 1); j++) {
+                if (i >= 0 && i < Minesweeper.getBoard().length && j >= 0 && j < Minesweeper.getBoard()[0].length) {
+                    if (Minesweeper.getBoard()[i][j].numMines.equals("0") && !Minesweeper.getBoard()[i][j].clicked) {
+                        Minesweeper.getBoard()[i][j].setClicked();
+                        Minesweeper.getBoard()[i][j].repaint();
+                        Minesweeper.getBoard()[i][j].flood(i, j);
+                    } else {
+                        Minesweeper.getBoard()[i][j].setClicked();
+                        Minesweeper.getBoard()[i][j].repaint();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (flagged) {
+            Font f = new Font("Times", Font.PLAIN, 40);
+            g.setFont(f);
+            FontMetrics fm = g.getFontMetrics();
+            int a = fm.getAscent();
+            int h = fm.getHeight();
+            int fl = fm.stringWidth("F");
+            g.setColor(new Color(170, 171, 167));
+            g.fillRect(0, 0, size, size);
+            g.setColor(Color.BLACK);
+            g.drawRect(0, 0, size, size);
+            g.setColor(Color.WHITE);
+            g.drawString("F", size / 2 - fl / 2, size / 2 + a - h / 2);
+        } else if (clicked) {
+            if (mine) {
+                g.setColor(Color.RED);
+                g.fillRect(0, 0, size, size);
+                g.setColor(Color.BLACK);
+                g.drawRect(0, 0, size, size);
+            } else {
+                if (!numMines.equals("0")) {
+                    Font f = new Font("Times", Font.PLAIN, 40);
+                    g.setFont(f);
+                    FontMetrics fm = g.getFontMetrics();
+                    int a = fm.getAscent();
+                    int h = fm.getHeight();
+                    g.setColor(Color.WHITE);
+                    g.fillRect(0, 0, size, size);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(0, 0, size, size);
+                    int w = fm.stringWidth(numMines);
+                    switch(numMines) {
+                        case "1":
+                            g.setColor(Color.BLACK);
+                            break;
+                        case "2":
+                            g.setColor(Color.BLUE);
+                            break;
+                        case"3":
+                            g.setColor(Color.RED);
+                            break;
+                        case "4":
+                            g.setColor(Color.GREEN);
+                            break;
+                        case "5":
+                            g.setColor(Color.ORANGE);
+                            break;
+                        case "6":
+                            g.setColor(Color.YELLOW);
+                            break;
+                        case "7":
+                            g.setColor(Color.CYAN);
+                            break;
+                        case "8":
+                            g.setColor(Color.PINK);
+                            break;
+                        default:
+                            break;
+                    }
+                    g.drawString(numMines, size / 2 - w / 2, size / 2 + a - h / 2);                    
+                } else {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(0, 0, size, size);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(0, 0, size, size);
+                }
+            }
+        } else {
+            g.setColor(new Color(170, 171, 167));
+            g.fillRect(1, 1, size - 2, size - 2);
+            g.setColor(Color.BLACK);
+            g.drawRect(0, 0, size, size);
+        }
+    }
+
+    private class MSMouseListener implements MouseListener {
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            //System.out.println("press");
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            //System.out.println("release");
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            //System.out.println("mouse entered");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            //System.out.println("mouse exited");
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if (SwingUtilities.isRightMouseButton(e)) {
+                if (!flagged && !clicked) {
+                    flagged = true;
+                } else if (flagged) {
+                    flagged = false;
+                } else {
+                }
+            } else if (SwingUtilities.isLeftMouseButton(e)) {
+                if (!flagged && !clicked) {
+                    clicked = true;
+                    repaint();
+                    if (mine) {
+                        lose();
+                    } else if (numMines.equals("0")) {
+                        flood(x, y);
+                    }
+                } else {
+                }
+                if (win()) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    System.exit(0);
+                }
+            }
+            repaint();
+        }
+    }
+}
